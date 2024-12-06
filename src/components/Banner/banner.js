b6f2553f4d680b182da4cf04ecc63cde3cba86fc
rodrigosdmenezes <rodrigosdmenezes@gmail.com>
2024-12-06 13:23:03 -0300

    Criando banner

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/assets/banner.png" alt="banner"/>
+        </header>
+    )
+
+}
+
+export default Banner
